950bdf84b0700ffb79dac0bb1bdc672101c07e39
drake_claimber <kostasusena@gmail.com>
2025-02-26 20:19:27 +0300

    Some new things

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 # Здесь будет основная работа с приложением, где будут выполняться методы POST и GET
+from flask import Flask, request, render_template 
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
