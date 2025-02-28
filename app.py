d5ea4acc620d078167a0ea588dba6a0b027935f2
drake_claimber <kostasusena@gmail.com>
2025-02-28 11:48:41 +0300

    Added everywhere Jinja2

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 # Здесь будет основная работа с приложением, где будут выполняться методы POST и GET
 import os
+from helper import login_required
 from flask import Flask, request, render_template, redirect, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
-
+# Создаём переменную, через которую будем работать с серверной частью сайта
 app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
-@app.route('/')
+# TODO Сделать Dashboard
+@app.route('/dashboard')
+@login_required
 def index():
-    return render_template('index.html')
+    return render_template('dashboard.html')
+
+# TODO Сделать Форму для регистрации 
+@app.route('/register')
+def register():
+    return render_template("/register.html")
+
+@app.route('/login')
+def login():
+    return render_template('/login.html')
+
--- helper.py
+++ helper.py
@@ -1,1 +1,15 @@
 # Здесь будут прописаны доп функции для app.py
+import requests
+
+from flask import redirect, render_template, session
+from functools import wraps
+
+# Функция для проверки регистрации
+def login_required(f):
+  @wraps(f)
+  def decarated_function(*args, **kwargs):
+    if session.get("user_id") is None:
+      return redirect('/login')
+    return f(*args, **kwargs)
+  
+  return decarated_function
