3b4ee72de067b9cb873afbf6fce203167831dada
drake_claimber <kostasusena@gmail.com>
2025-03-03 21:34:50 +0300

    fod

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from werkzeug.security import check_password_hash, generate_password_hash
 # Создаём переменную, через которую будем работать с серверной частью сайта
 app = Flask(__name__)
 
-app.config["SESSION PERMANENT"] = False
-app.config["SESSION TYPE"] = "filesystem"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 if __name__ == '__main__':
